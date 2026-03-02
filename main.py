62460d968e4eaaa94d657a868f6040e33ce2c16a
O Olajide <oo169@student.le.ac.uk>
2026-03-02 02:00:15 +0000

    switched to ollama models

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+
 
 load_dotenv()
 
@@ -10,21 +11,18 @@ def main():
     text = "What is the capital of Nigeria?"
 
     # Same as using s strings directly
-    prompt = PromptTemplate(input_variables=["question"], template="summary_template {question}")
+    prompt = PromptTemplate(
+        input_variables=["question"], template="Answer the following question: {question}"
+    )
 
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-3-pro-preview",
-        temperature=1.0,  # Gemini 3.0+ defaults to 1.0
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-        # other params...
+    llm = ChatOllama(
+        model="gemma3:270m",
+        temperature=0,
     )
 
     chain = prompt | llm
 
     response = chain.invoke({"question": text})
-
     print(response.content)
 
 if __name__ == "__main__":
