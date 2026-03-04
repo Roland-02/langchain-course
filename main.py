82c0a1b9728febe1a17b309bd13ea443f200f0bf
O Olajide <oo169@student.le.ac.uk>
2026-03-04 22:27:27 +0000

    intro to search agents

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-
+from langchain_tavily_search import TavilySearch
 
 load_dotenv()
 
+@tool
+def search(query: str) -> str:
+    """Search the web for information"""
+    print(f"Searching the web for {query}")
+    return "This is a test search result"
+
+
+llm = ChatOllama(model="gemma3:270m", temperature=0)
+# tools = [TavilySearch()] # Tavily is built-in tool for searching the web
+tools = [search] # Custom search tool
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
     print("Hello from langchain-course!")
-
-    text = "What is the capital of Nigeria?"
-
-    # Same as using s strings directly
-    prompt = PromptTemplate(
-        input_variables=["question"], template="Answer the following question: {question}"
-    )
-
-    llm = ChatOllama(
-        model="gemma3:270m",
-        temperature=0,
-    )
-
-    chain = prompt | llm
-
-    response = chain.invoke({"question": text})
-    print(response.content)
+    result = agent.invoke({"input": "What is the capital of France?"})
+    print(result)
 
 if __name__ == "__main__":
     main()
